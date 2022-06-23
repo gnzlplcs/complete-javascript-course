c056a3a5b3b364fcf1abe43a29e428749b6df2db
gnzlplcs <palaciosgm@outlook.com>
2022-06-23 07:27:02 -0500

    lesson 41: coding challenge #2 completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -66,31 +66,74 @@ GOOD LUCK 😀
 ////////////////////////////////
 // intro to ARRAYS: methods
 
-// push() method
-const friends = ['Michael', 'Steven', 'Peter'];
-const newLengthFriends = friends.push('Jay')
-console.log(friends)
-console.log(newLengthFriends) // returns 4
+// // push() method
+// const friends = ['Michael', 'Steven', 'Peter'];
+// const newLengthFriends = friends.push('Jay')
+// console.log(friends)
+// console.log(newLengthFriends) // returns 4
 
-// unshift() method
-const returnsSomething = friends.unshift('John')
-console.log(friends)
-console.log(returnsSomething) // returns 5
+// // unshift() method
+// const returnsSomething = friends.unshift('John')
+// console.log(friends)
+// console.log(returnsSomething) // returns 5
 
-// pop()
-const popped = friends.pop()
-console.log(popped) // returns 'Jay'
-console.log(friends)
+// // pop()
+// const popped = friends.pop()
+// console.log(popped) // returns 'Jay'
+// console.log(friends)
 
-// shift()
-const shifted = friends.shift()
-console.log(shifted) // returns 'John'
-console.log(friends)
+// // shift()
+// const shifted = friends.shift()
+// console.log(shifted) // returns 'John'
+// console.log(friends)
 
-// indexOf()
-console.log(friends.indexOf('Steven')) // returns: 1
-console.log(friends.indexOf('Gonzalo')) // returns: -1
+// // indexOf()
+// console.log(friends.indexOf('Steven')) // returns: 1
+// console.log(friends.indexOf('Gonzalo')) // returns: -1
 
-// includes()
-console.log(friends.includes('Gonzalo')) // returns: false
-console.log(friends.includes('Steven')) // returns: true
+// // includes()
+// console.log(friends.includes('Gonzalo')) // returns: false
+// console.log(friends.includes('Steven')) // returns: true
+
+// --------------------------------------------------------
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+const calcTip = (value) => {
+  if (value >= 50 && value <= 300) {
+    return (value * 0.15).toFixed(2);
+  } else {
+    return (value * 0.2).toFixed(2);
+  }
+}
+
+const bills = [125, 555, 44]
+
+let tips = []
+let total = []
+
+for (let index = 0; index < bills.length; index++) {
+  let tip = Number(calcTip(bills[index]))
+  let totalPerBill = bills[index] + tip
+  tips.push(tip)
+  total.push(totalPerBill)
+}
+
+console.log(tips)
+console.log(total)
